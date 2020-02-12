e734ca1896c9c6fb7d677ed2def517a31b73fedf
kazukazu0822 <34573252+kazukazu0822@users.noreply.github.com>
2020-02-12 20:37:32 +0900

    Add files via upload

---
+++ Sample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            this.GetComponent<Inventory>().SetItemCommandTemprate(1, 1);
+            Debug.Log("1,1のアイテムが追加されました。");
+        }
+    }
+}
